67f50767f859fc43c4b7ea747516b3ac1dfab982
Matthew Saber <mattsaber21@yahoo.com>
2021-03-31 23:35:59 -0700

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
 // import functions and grab DOM elements
-const fightButton = document.getElementById('fight');
 // initialize state
 
 // set event listeners to update state and DOM
-const = addEventListener('click',
+
+
+
+
+import { whoIsTheVictor, getComputerChampion } from './utils.js';
+
+const battleButton = document.querySelector('#battle-button');
+const victorText = document.querySelector('#Victor');
+const Rock = document.querySelector('#Rock');
+const Paper = document.querySelector('#Paper');
+const Scissors = document.querySelector('#Scissors');
+const successText = document.querySelector('#Success');
+const defeatText = document.querySelector('#Defeat');
+const drawText = document.querySelector('#draws');
+const totalAnswers = document.querySelector('#total');
+
+let Success = 0;
+let Defeat = 0;
+let draws = 0;
+let total = 0;
+
+battleButton.addEventListener('click', () => {
+    const player = document.querySelector('input:checked').value;
+    const computer = getComputerChampion();
+    const victor = whoIsTheVictor(player, computer);
+    if (victor === 'Your Might is Equal') {
+        draws++;
+        drawText.textContent = `${draws} Your Might is Equal`;
+        total++;
+        totalAnswers.textContent = `${total} total`;
+    }
+    if (victor === 'Your Family Mourns You') {
+        Defeat++;
+        defeatText.textContent = `${Defeat} Your Family Mourns You`;
+        total++;
+        totalAnswers.textContent = `${total} total`;
+    }
+    if (victor === 'Your Have Vanquished Your Foe') {
+        Success++;
+        successText.textContent = `${Success} Your Have Vanquished Your Foe`;
+        total++;
+        totalAnswers.textContent = `${total} total`;
+    }
+});
--- utils.js
+++ utils.js
@@ -2,12 +2,12 @@ export function getComputerChampion() {
     let randomNumber = Math.ceil(Math.random()); {
         let computerChampion;
         if (randomNumber >= 0 && randomNumber <= 0.325)
-            computerChampion === 'rock';
+            computerChampion === 'Rock';
 
         else if (randomNumber >= 0.3251 && randomNumber <= 0.65)
-            computerChampion === 'paper';
+            computerChampion === 'Paper';
 
-        else computerChampion === 'scissors';
+        else computerChampion === 'Scissors';
 
     }
     return computerChampion;
@@ -24,22 +24,37 @@ export function whoIsTheVictor(user, computer) {
     //let computer = computerChampion
 
     if (user === 'rock' && computer === 'rock') {
-        return 'Your Might is Equal';
+        return 'Your Might is Equal'
 
-    } else if (user === 'paper' && computer === 'paper') {
-        return 'Your Might is Equal';
+    };
+    if (user === 'paper' && computer === 'paper') {
+        return 'Your Might is Equal'
 
-    } else if (user === 'scissors' && computer === 'scissors') {
-        return 'Your Might is Equal';
+    };
+    if (user === 'scissors' && computer === 'scissors') {
+        return 'Your Might is Equal'
 
-    } else if (user === 'rock' && computer === 'scissor') {
-        return 'Your Have Vanquished Your Foe';
+    };
+    if (user === 'rock' && computer === 'scissors') {
+        return 'Your Have Vanquished Your Foe'
 
-    } else if (user === 'paper' && computer === 'rock') {
-        return 'Your Have Vanquished Your Foe';
+    };
+    if (user === 'paper' && computer === 'rock') {
+        return 'Your Have Vanquished Your Foe'
 
-    } else if (user === 'scissors' && computer === 'paper') {
-        return 'Your Have Vanquished Your Foe';
-    } else return 'Your Family Mourns You';
-}
+    };
+    if (user === 'scissors' && computer === 'paper') {
+        return 'Your Have Vanquished Your Foe'
 
+    };
+    if (user === 'scissors' && computer === 'rock') {
+        return 'Your Family Mourns You'
+
+    };
+    if (user === 'paper' && computer === 'scissors') {
+        return 'Your Family Mourns You'
+
+    };
+    if (user === 'rock' && computer === 'paper') {
+        return 'Your Family Mourns You'
+    };
